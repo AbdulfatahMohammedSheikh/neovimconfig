c896387ae91825c745d40cb5170f7db50165743a
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2023-04-18 17:09:43 +0300

    adding more remaps for renameing and jump in lists

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,11 +1,11 @@
-local  nnoremap = require("user.keymap").nnoremap
-local set_keymap= function(mode, key, result)
-  vim.api.nvim_set_keymap(
-    mode,
-    key,
-    result,
-    {noremap = true, silent = true}
-  )
+local nnoremap = require("user.keymap").nnoremap
+local set_keymap = function(mode, key, result)
+    vim.api.nvim_set_keymap(
+        mode,
+        key,
+        result,
+        { noremap = true, silent = true }
+    )
 end
 -- initial settup and mappting
 set_keymap('i', 'jj', '<ESC>')
@@ -19,16 +19,18 @@ set_keymap('n', '<leader>p', ':Ex<CR>')
 set_keymap('n', '<leader>t', ':so<CR>')
 set_keymap('n', '<leader>v', ':belowright terminal<CR>')
 
- --     Windows and navigation 
- --
- -- > Resizing window
+-- for fast rename and refactoring
+set_keymap('n', '<leader>h', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<left><left><left>]])
+--     Windows and navigation
+--
+-- > Resizing window
 
 set_keymap('n', '<C-UP>', ':resize -2 <CR>')
 set_keymap('n', '<C-DOWN>', ':resize +2 <CR>')
 set_keymap('n', '<C-lEFT>', ':vertical resize -2 <CR>')
 set_keymap('n', '<C-RIGHT>', ':vertical resize +2 <CR>')
 
- -- >  splites
+-- >  splites
 
 set_keymap('n', 'sv', ':split<CR>')
 set_keymap('n', 's;', ':vsplit<CR>')
@@ -38,30 +40,31 @@ set_keymap('n', 'sk', '<C-w>k')
 set_keymap('n', 'sl', '<C-w>l')
 set_keymap('n', 'sh', '<C-w>h')
 
- -- >  Split navigation
+-- >  Split navigation
 set_keymap('n', '<leader>b', ':bd<CR>')
 
- --     Code props   
- -- 
- -- > Quickfix list
- --
+--     Code props
+--
+-- > Quickfix list
+--
 
 set_keymap('n', '<leader>q', ':copen<CR>')
 set_keymap('n', '<leader>k', ':cprev<CR>')
 set_keymap('n', '<leader>j', ':cnext<CR>')
+set_keymap('n', '<leader>l', ':lopen<CR>')
 
- -- >  Formating 
- --
+-- >  Formating
+--
 
 set_keymap('n', '<C-j>', ':m .+1<CR>')
 set_keymap('n', '<C-k>', ':m .-2<CR>')
 
- -- >  chage file permesions 
- --
+-- >  chage file permesions
+--
 set_keymap('n', '<leader>x', ':!chmod +x %<CR>')
 
- -- >  Telescope 
- --
+-- >  Telescope
+--
 
 -- Find files using Telescope command-line sugar.
 set_keymap('n', '<leader>ff', ':Telescope find_files<CR>')
@@ -70,8 +73,8 @@ set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>')
 set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>')
 
 
- -- >  Dap and Dap-ui 
- --
+-- >  Dap and Dap-ui
+--
 
 
 set_keymap('n', '<leader>d', ':DapContinue<CR>')
@@ -80,7 +83,7 @@ set_keymap('n', '<leader>dt', ':lua require("dapui").toggle()<CR>')
 set_keymap('n', '<leader>de', ':DapTerminate<CR>')
 -- Gernal lsp maps for all languages
 --
-local bufopts = { noremap=true, silent=true, buffer=bufnr }
+local bufopts = { noremap = true, silent = true, buffer = bufnr }
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
 vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, bufopts)
@@ -94,23 +97,23 @@ vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.references, bufopts)
-vim.keymap.set('n', ';f', vim.lsp.buf.format , bufopts)
+vim.keymap.set('n', ';f', vim.lsp.buf.format, bufopts)
 
--- harpoon 
+-- harpoon
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 
-vim.keymap.set("n", "<leader>i",ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>a",mark.add_file)
+vim.keymap.set("n", "<leader>i", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
 
--- Flutter 
+-- Flutter
 
-vim.keymap.set("n", "<leader>fs",":FlutterRestart<CR>")
-vim.keymap.set("n", "<leader>fr",":FlutterReload<CR>")
-vim.keymap.set("n", "<leader>fd",":FlutterDevices<CR>")
-vim.keymap.set("n", "<leader>ft",":FlutterDevTools<CR>")
+vim.keymap.set("n", "<leader>fs", ":FlutterRestart<CR>")
+vim.keymap.set("n", "<leader>fr", ":FlutterReload<CR>")
+vim.keymap.set("n", "<leader>fd", ":FlutterDevices<CR>")
+vim.keymap.set("n", "<leader>ft", ":FlutterDevTools<CR>")
