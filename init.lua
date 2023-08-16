169b0f38a4694d2c9fda03da2789872c4364920d
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2023-08-16 13:35:08 +0300

    fixed: lsp proble due to luasnap

--- init.lua
+++ init.lua
@@ -50,22 +50,20 @@ require('lazy').setup({
     'mfussenegger/nvim-dap',
     { "rcarriga/nvim-dap-ui" },
 
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
-    -- use({
     --    "hrsh7th/nvim-cmp",
     --   config = [[require('config.nvim-cmp')]],
-    --  dependencies = {
-    --     "saadparwaiz1/cmp_luasnip",
-    --    "hrsh7th/cmp-nvim-lua",
-    --   "hrsh7th/cmp-nvim-lsp",
-    --    "hrsh7th/cmp-path",
-    --     "hrsh7th/cmp-buffer",
-    --    "hrsh7th/cmp-omni",
-    --  },
-    --})
-    -- 'hrsh7th/cmp-vsnip',
-    -- 'hrsh7th/vim-vsnip',
+    dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-omni",
+    },
     { 'akinsho/flutter-tools.nvim', dependencies = 'nvim-lua/plenary.nvim' },
     {
         'nvim-telescope/telescope.nvim',
@@ -120,7 +118,13 @@ require('lazy').setup({
     },
 
 
-    { 'saadparwaiz1/cmp_luasnip' },
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        -- install jsregexp (optional!).
+        build = "make install_jsregexp"
+    },
     {
         "L3MON4D3/LuaSnip",
         -- follow latest release.
--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,2 +1,1 @@
-require("user.set")
 require("user.remap")
