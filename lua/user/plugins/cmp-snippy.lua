6038d4c5f75c2eeeb29608a2753ee94039a74351
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2024-01-06 15:26:05 +0300

    added: snippy as snippets engine

---
+++ lua/user/plugins/cmp-snippy.lua
@@ -0,0 +1,16 @@
+return {
+    "dcampos/cmp-snippy",
+    config = function()
+        -- require 'cmp'.setup {
+        --     snippet = {
+        --         expand = function(args)
+        --             require 'snippy'.expand_snippet(args.body)
+        --         end
+        --     },
+        --
+        --     sources = {
+        --         { name = 'snippy' }
+        --     }
+        -- }
+    end
+}
