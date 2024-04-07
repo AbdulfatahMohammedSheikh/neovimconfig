9f89cd43b264abfaab7534de236d6e14fd99cd78
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2024-04-07 23:04:56 +0300

    added: solarized-osaka.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,7 @@ require('user.core')
 require('user.lazy')
 require('user.snippets')
 
--- vim.cmd('colorscheme nightfly')
-vim.cmd.colorscheme 'midnight'
+vim.cmd("colorscheme solarized-osaka")
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Hightlight yanked test",
     group = vim.api.nvim_create_augroup("Yanked test", { clear = true }),
