4fe34bab634dd615af00619f9438c8004fa6f1b1
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2024-04-07 22:48:40 +0300

    added new theme

--- init.lua
+++ init.lua
@@ -2,8 +2,8 @@ require('user.core')
 require('user.lazy')
 require('user.snippets')
 
-vim.cmd("colorscheme solarized-osaka")
-
+-- vim.cmd('colorscheme nightfly')
+vim.cmd.colorscheme 'midnight'
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Hightlight yanked test",
     group = vim.api.nvim_create_augroup("Yanked test", { clear = true }),
