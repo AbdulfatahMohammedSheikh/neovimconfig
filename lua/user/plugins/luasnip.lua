84e720a03e1191fe5afdb2f5cc6fd03bec7c75d7
abdulfatah mohammed sheikh <abdulfatahmohammedsheikh@gmail.com>
2024-04-11 20:45:08 +0300

    added: c-k to select snippet

--- lua/user/plugins/luasnip.lua
+++ lua/user/plugins/luasnip.lua
@@ -13,35 +13,23 @@ return {
 
         vim.cmd([[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]) --}}}
 
+
         -- Virtual Text{{{
-        local types = require("luasnip.util.types")
         ls.config.set_config({
             history = true,                            --keep around last snippet local to jump back
             updateevents = "TextChanged,TextChangedI", --update changes as you type
             enable_autosnippets = true,
-            ext_opts = {
-                [types.choiceNode] = {
-                    active = {
-                        virt_text = { { "●", "GruvboxOrange" } },
-                    },
-                },
-                -- [types.insertNode] = {
-                -- 	active = {
-                -- 		virt_text = { { "●", "GruvboxBlue" } },
-                -- 	},
-                -- },
-            },
         }) --}}}
 
         -- Key Mapping --{{{
 
-        vim.keymap.set({ "i", "s" }, "snu", '<cmd>lua require("luasnip.extras.select_choice")()<cr><C-c><C-c>')
 
         vim.keymap.set({ "i", "s", "n" }, "snp", function()
             if ls.expand_or_jumpable() then
                 ls.expand()
             end
         end, { silent = true })
+        vim.keymap.set({ "i" }, "<C-k>", function() ls.expand() end, { silent = true })
         -- vim.keymap.set({ "i", "s" }, "<C-k>", function()
         -- 	if ls.expand_or_jumpable() then
         -- 		ls.expand_or_jump()
@@ -69,11 +57,6 @@ return {
         vim.keymap.set({ "i", "s", "n" }, "snj", function()
             if ls.choice_active() then
                 ls.change_choice(1)
-            else
-                -- print current time
-                local t = os.date("*t")
-                local time = string.format("%02d:%02d:%02d", t.hour, t.min, t.sec)
-                print(time)
             end
         end)
         vim.keymap.set({ "i", "s", "n" }, "snk", function()
